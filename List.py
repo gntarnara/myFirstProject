5b94354245bdce9125dac2e8c39d61f157e72684
gtarnara <g.tarnaraa@gmail.com>
2025-10-23 19:18:48 +0300

    Exercise in Lists

---
+++ List.py
@@ -0,0 +1,11 @@
+ar = ["a", "b", "c"]
+
+for i in range(len(ar)):
+    print(i, ar[i])
+
+# for a in ar:
+    # print(a)
+print("Reversed")
+
+for i in range(len(ar)-1, -1, -1):
+    print(i, ar[i])
